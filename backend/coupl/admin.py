b5a18d4fd182daf548303f73c3880be417b258ac
scavbob <cankatkadim@gmail.com>
2021-12-10 18:12:21 +0300

    Added the models to admin page and most models are created to at least a bare-bones state

--- backend/coupl/admin.py
+++ backend/coupl/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Coordinator, Event, Comment, Rating, SubAreas
 
 # Register your models here.
+admin.site.register(Coordinator)
+admin.site.register(Event)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(SubAreas)
--- backend/coupl/models.py
+++ backend/coupl/models.py
@@ -2,22 +2,37 @@ import phonenumber_field.modelfields
 from django.contrib.auth.models import User
 from django.db import models
 from phonenumber_field import modelfields
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
-    profileName = models.CharField(blank=False, max_length=30)
-    profileSurname = models.CharField(blank=False, max_length=30)
-    profilePhone = modelfields.PhoneNumberField(blank=False)
-    profileDateOfBirth = models.DateField(blank=False, editable=False)
-    profileEventHistory = None
-    profileMatchHistory = None
-    profileDescription = models.CharField(default="", max_length=200)
-    profilePhotos = None
-    profileGender = models.CharField(blank=False)  # "Male" or "Female" written on db
-    profilePreference = models.CharField(blank=False)  # Will receive "Male", "Female" or "Both" from the front-end and
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(blank=False, max_length=30)
+    surname = models.CharField(blank=False, max_length=30)
+    phone = modelfields.PhoneNumberField(blank=False)
+    dateOfBirth = models.DateField(blank=False, editable=False)
+    description = models.CharField(default="", max_length=200)
+    photos = None
+    gender = models.CharField(blank=False, max_length=10)  # "Male" or "Female" written on db
+    preference = models.CharField(blank=False,
+                                  max_length=10)  # Will receive "Male", "Female" or "Both" from the front-end and
+
     # place what is received into the db
+    @property
+    def eventHistory(self):
+        return None
+
+    @property
+    def matchHistory(self):
+        return None
+
+
+class Location(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    description = models.CharField(max_length=100)
+    address = models.CharField(blank=False, max_length=150)
+    # photo = models.ImageField()
 
 
 class Event(models.Model):
@@ -26,17 +41,54 @@ class Event(models.Model):
     eventTags = models.ManyToManyField("Tag")
     eventStartTime = models.DateTimeField(blank=False)
     eventFinishTime = models.DateTimeField(blank=False)
-    eventCreator = models.ForeignKey("User", on_delete=models.CASCADE)
+    eventCreator = models.ForeignKey("Coordinator", on_delete=models.CASCADE)
     eventLocation = models.ForeignKey("Location", on_delete=models.CASCADE)
-    eventSubAreas = None
-    eventRating = None
-    eventComment = None
-    eventQRCode = None
-    eventAttendees = models.ManyToManyField("User")
+    eventAttendees = models.ManyToManyField(User)
+
+    @property
+    def eventQRCode(self):
+        return None
+
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    commentText = models.CharField(max_length=150)
+
+
+class Rating(models.Model):
+    class Stars(models.IntegerChoices):
+        FIVE_STARS = 5, _('*****')
+        FOUR_STARS = 4, _('****')
+        THREE_STARS = 3, _('***')
+        TWO_STARS = 2, _('**')
+        ONE_STAR = 1, _('*')
+
+    rating = models.IntegerField(choices=Stars.choices)
+    rater = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    tagName = models.CharField(blank=False, max_length=50)
+    tagDescription = models.CharField(max_length=100)
+
+
+class SubAreas(models.Model):
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    areaName = models.CharField(blank=False, max_length=50)
+    areaDescription = models.CharField(max_length=100)
+    areaPicture = None
 
 
 class Coordinator(models.Model):
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     coordinatorPhone = modelfields.PhoneNumberField(blank=False)
     coordinatorContact = None
     coordinatorRating = None
+
+
+class Ticket(models.Model):
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    reported = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(blank=False, max_length=250)
