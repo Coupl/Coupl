0d347a6e872ee105b4ead6881931283b1b8c9efb
Ruzgar <zzlawlzz52@gmail.com>
2021-12-11 22:14:40 +0300

    Simple and single page register with no styles

--- android/index.js
+++ android/index.js
@@ -7,15 +7,23 @@ import App from './App';
 import React from 'react';
 import { name as appName } from './app.json';
 import configureStore from './src/redux/reducers/configureStore';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
+import {
+    enGB,
+    registerTranslation,
+  } from 'react-native-paper-dates'
 
 const store = configureStore();
+registerTranslation('en-GB', enGB);
 
 const WrappedApp = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <StoreProvider store={store}>
+        <PaperProvider>
+            <App />
+        </PaperProvider>
+    </StoreProvider>
 );
 
 AppRegistry.registerComponent(appName, () => WrappedApp);
