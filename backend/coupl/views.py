0a259128ed055a080e98a0a2ada63cea6f29e864
scavbob <cankatkadim@gmail.com>
2021-12-12 21:19:33 +0300

    added requirements.txt

--- backend/coupl/views.py
+++ backend/coupl/views.py
@@ -1,13 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import authentication, permissions
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import viewsets
 
 from coupl.serializers import UserSerializer
 
