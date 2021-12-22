51457fb11ff3c3f31e15d972bdcaf006ae4ad73f
kaanerkan <kaan.erkan@ug.bilkent.edu.tr>
2021-12-23 00:11:04 +0300

    C2-18 User upcoming events list

--- android/src/components/User/UpcomingEventsScreen.js
+++ android/src/components/User/UpcomingEventsScreen.js
@@ -1,13 +1,73 @@
-
 import React from 'react';
 import {
+    View,
     Text,
+    Image,
+    StyleSheet,
+    Dimensions,
+    FlatList,
+    TouchableOpacity,
+    SafeAreaView
 } from 'react-native';
+import { data } from "./data";
+
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
 const UpcomingEventsScreen = ({ navigation }) => {
     return (
-        <Text>Burası gelecek etkinlikler ekranı</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+                data={data}
+                keyExtractor={item => item.key}
+                contentContainerStyle={{ padding: 16 }}
+                renderItem={item => {
+                    return (
+                        <TouchableOpacity style={{ marginBottom: 16, height: height * 0.40 }} onPress={() => { }}>
+                            <View style={{ flex: 1, padding: 16 }}>
+                                <View
+                                    style={
+                                        [StyleSheet.absoluteFillObject,
+                                        { backgroundColor: '#C0D6E4', borderRadius: 16 },
+                                        ]}
+                                />
+                                <Image style={styles.image} source={{ uri: item.item.image }} />
+                                <Text style={styles.name}>{item.item.name}</Text>
+                                <Text style={styles.description}>Location: {item.item.location}</Text>
+                                <Text style={styles.description}>Date: {item.item.date}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }}
+            />
+        </SafeAreaView>
     );
 };
 
 export default UpcomingEventsScreen;
+
+const styles = StyleSheet.create({
+    name: {
+        marginTop: -12,
+        fontWeight: '700',
+        fontSize: 18,
+        color: "black",
+    },
+    description: {
+        paddingTop: 2,
+        paddingBottom: 2,
+        fontSize: 14,
+        opacity: 0.7
+    },
+    image: {
+        marginLeft: -16,
+        marginTop: -42,
+        width: width * 0.92,
+        height: height * 0.31,
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        resizeMode: 'contain',
+    }
+
+});
---
+++ android/src/components/User/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    key: 1,
+    name: 'Random Event 1',
+    description: 'Description 1',
+    date: '10.12.2021',
+    startTime: '20.00',
+    endTime: '23.00',
+    location: 'Ankara',
+    image:
+      'https://klinar-cro.com/wp-content/uploads/2019/01/new_statesman_events.jpg'
+  },
+  {
+    key: 2,
+    name: "Random Event 2",
+    description: 'Description 2',
+    date: '10.12.2021',
+    startTime: '20.00',
+    endTime: '23.00',
+    location: 'Ankara',
+    image:
+      'https://klinar-cro.com/wp-content/uploads/2019/01/new_statesman_events.jpg'
+  },
+  {
+    key: 3,
+    name: 'Random Event 3',
+    description: 'Description 3',
+    date: '10.12.2021',
+    startTime: '20.00',
+    endTime: '23.00',
+    location: 'Ankara',
+    image:
+      'https://klinar-cro.com/wp-content/uploads/2019/01/new_statesman_events.jpg'
+  },
+  {
+    key: 4,
+    name: 'Random Event 4',
+    description: 'Description 4',
+    date: '10.12.2021',
+    startTime: '20.00',
+    endTime: '23.00',
+    location: 'Ankara',
+    image:
+      'https://klinar-cro.com/wp-content/uploads/2019/01/new_statesman_events.jpg'
+  },
+];
