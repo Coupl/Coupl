343c5cea76b537c9ec50e6564de9225c9248e7ec
Ruzgar <zzlawlzz52@gmail.com>
2021-12-25 20:00:18 +0300

    Add QR Code Scanner

--- android/src/components/User/HomeScreen.js
+++ android/src/components/User/HomeScreen.js
@@ -1,17 +1,31 @@
 
 import React from 'react';
 import {
-    Text,
+    Dimensions,
+    StyleSheet,
+    Text, TouchableOpacity, View,
 } from 'react-native';
 import { Button } from 'react-native-paper';
 import { useStore } from 'react-redux';
 import allActions from '../../redux/actions';
 import axios from 'axios'
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import { useIsFocused } from '@react-navigation/native';
+import AntDesign from "react-native-vector-icons/AntDesign";
+import { useState } from 'react';
+import { render } from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod';
+
+const QRCodeScannerTimeout = 1000;
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
+
 const HomeScreen = ({ navigation }) => {
 
     const store = useStore();
     const joinEventAction = allActions.eventActions.joinEvent;
+    const [renderScanner, setRenderScanner] = useState(false);
 
     const joinEvent = (eventId) => {
         axios.get('getEvent?event_id=' + eventId).then((res) => {
@@ -24,17 +38,96 @@ const HomeScreen = ({ navigation }) => {
         });
     }
 
+    const onRead = e => {
+        const url = e.data;
+        const eventId = url.split("=")[1];
+
+        const eventInfo = {
+            eventName: "Rastgele bir etkinlik",
+            eventDescription: "eventDescription",
+            numParticipants: 25,
+            eventFinishTime: "eventFinishTime"
+        }
+        store.dispatch(joinEventAction(eventInfo));
+        navigation.navigate('EventNavigation');
+        //joinEvent(eventId);
+
+        setRenderScanner(false);
+    }
+
+    const onPreviewClick = () => {
+        setRenderScanner(true);
+    }
+
+    const QRScanner = () => {
+        const isFocused = useIsFocused();
+        return (
+            <>
+                {isFocused && <QRCodeScanner
+                    onRead={onRead}
+                    flashMode={RNCamera.Constants.FlashMode.torch}
+                    reactivate={true} //Can be used again
+                    reactivateTimeout={QRCodeScannerTimeout}
+                    /*
+                    bottomContent={
+                        <Button
+                            mode="contained"
+                            onPress={() => joinEvent(7)}
+                        >
+                            Check Out the Upcoming Events
+                        </Button>
+                    }
+                    */
+                />
+                }
+            </>
+        )
+    }
+
+    const Preview = () => {
+        return (
+            <>
+                <TouchableOpacity style={styles.qrScannerPreview} onPress={onPreviewClick}>
+                    <AntDesign name="camera" size={120}
+                        color={'#fff'}
+                    >
+                    </AntDesign>
+                </TouchableOpacity>
+
+                <Text style={styles.text}>Enter an Event by Scanning a QR Code</Text>
+                <Text style={styles.text}>OR</Text>
+                <Button
+                    mode="contained"
+                    onPress={() => navigation.navigate('UpcomingEventsScreen')}
+                >
+                    Check Out the Upcoming Events
+                </Button>
+            </>
+        )
+    }
+    
     return (
-        <>
-            <Text>Burası ana ekran, analiz raporundaki ui resimlerine göre sadece qr code tarama şeyi olacak</Text>
-            <Button
-                mode="contained"
-                onPress={() => joinEvent(7)}
-            >
-                Test için rastgele evente girme tuşu
-            </Button>
-        </>
+        <View style={styles.container}>
+            {renderScanner ? <QRScanner /> : <Preview />}
+        </View>
     );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+    },
+    qrScannerPreview: {
+        backgroundColor: "black",
+        width: width,
+        height: height * 0.7,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
