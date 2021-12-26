9d88293812410fbe0791d4a6fd11fd6cd62fe3bc
scavbob <cankatkadim@gmail.com>
2021-12-26 22:55:19 +0300

    ListProfileView

--- backend/coupl/serializers.py
+++ backend/coupl/serializers.py
@@ -4,21 +4,6 @@ from rest_framework import serializers
 from coupl.models import Profile, Event, Tag
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['user', 'name', 'surname', 'phone', 'dateOfBirth', 'description',
-                  'photos', 'gender', 'preference']
-
-    def create(self, validated_data):
-        return Profile.objects.create(user=validated_data.get('user'), name=validated_data.get('name'),
-                                      surname=validated_data.get('surname'),
-                                      phone=validated_data.get('phone'), dateOfBirth=validated_data.get('dateOfBirth'),
-                                      description=validated_data.get('description'),
-                                      gender=validated_data.get('gender'), preference=validated_data.get('preference'))
-    #def update TO BE IMPLEMENTED
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -58,9 +43,21 @@ class UserDisplaySerializer(serializers.RelatedField):
         return {"pk": pk, "username": username}
 
 
-class ProfileDisplaySerializer(serializers.RelatedField):
-    def to_representation(self, value):
-        return None
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'name', 'surname', 'phone', 'dateOfBirth', 'description',
+                  'photos', 'gender', 'preference']
+
+    def create(self, validated_data):
+        return Profile.objects.create(user=validated_data.get('user'), name=validated_data.get('name'),
+                                      surname=validated_data.get('surname'),
+                                      phone=validated_data.get('phone'), dateOfBirth=validated_data.get('dateOfBirth'),
+                                      description=validated_data.get('description'),
+                                      gender=validated_data.get('gender'), preference=validated_data.get('preference'))
+    #def update TO BE IMPLEMENTED
 
 
 class TagDisplaySerializer(serializers.RelatedField):
